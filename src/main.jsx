03e8096cbd96c35297d1ee4b513db473a8849184
gussaay <gussaay@gmail.com>
2025-08-15 19:25:48 +0200

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import AuthGate from './auth-gate.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthGate>
       <App />
     </AuthGate>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 2721d1f7321ee81f1f040962e0f5c156b71ac7cb
