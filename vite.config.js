d7be2117329eb670c229219880a8bac78131fd3f
gussaay <gussaay@gmail.com>
2025-08-15 22:48:13 +0200

    fix: Set base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This line is required for Firebase Hosting
   define: {
     // Pass through a JSON string for production builds on App Hosting
     FIREBASE_WEBAPP_CONFIG: process.env.FIREBASE_WEBAPP_CONFIG
